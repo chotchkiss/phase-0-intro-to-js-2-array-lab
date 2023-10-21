e4430aeb022e96018f1eb50eb323009f20bce1c4
Cheyenne Hotchkiss <cheycheymh@gmail.com>
2023-10-21 19:42:09 -0400

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat(name) {
+    return cats.concat("Broom");
+}
+let copyOfCats = [...cats];
+function prependCat(name) {
+    return(copyOfCats = ["Arnold", ...cats]);
+}
+function removeLastCat() {
+    return(cats.slice(0, 2));
+}
+function removeFirstCat() {
+    return(cats.slice(1, 3));
+}
